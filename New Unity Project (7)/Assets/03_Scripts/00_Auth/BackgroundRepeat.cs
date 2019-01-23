2697a2960d1580ad52afadc277c19ff66d512f16
Cory Kim <corykim0829@gmail.com>
2019-01-23 18:14:05 +0900

    feat: background before firebase

--- New Unity Project (7)/Assets/03_Scripts/00_Auth/BackgroundRepeat.cs
+++ New Unity Project (7)/Assets/03_Scripts/00_Auth/BackgroundRepeat.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class BackgroundRepeat : MonoBehaviour {
-    public const float scrollSpeed = 1.2f; 
+    public float scrollSpeed = 1.2f; 
     //스크롤할 속도를 상수로 지정해 줍니다.
     private Material thisMaterial;
     //Quad의 Material 데이터를 받아올 객체를 선언합니다.
@@ -15,7 +15,7 @@ public class BackgroundRepeat : MonoBehaviour {
     void Update () {
         Vector2 newOffset = thisMaterial.mainTextureOffset;
         // 새롭게 지정해줄 OffSet 객체를 선언합니다.
-        newOffset.Set(0, newOffset.y + (scrollSpeed * Time.deltaTime));
+        newOffset.Set(newOffset.x + (scrollSpeed * Time.deltaTime), 0);
         // Y부분에 현재 y값에 속도에 프레임 보정을 해서 더해줍니다.
         thisMaterial.mainTextureOffset = newOffset;
         //그리고 최종적으로 Offset값을 지정해줍니다.
