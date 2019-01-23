31a855baafb5030eb10030f40cd874d16822d4e2
Cory Kim <corykim0829@gmail.com>
2019-01-23 17:35:12 +0900

    feat: background scroll basic

---
+++ New Unity Project (7)/Assets/03_Scripts/00_Auth/BackgroundRepeat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRepeat : MonoBehaviour {
+    public const float scrollSpeed = 1.2f; 
+    //스크롤할 속도를 상수로 지정해 줍니다.
+    private Material thisMaterial;
+    //Quad의 Material 데이터를 받아올 객체를 선언합니다.
+    void Start () {
+        //객체가 생성될때 최초 1회만 호출 되는 함수 입니다.
+        thisMaterial = GetComponent<Renderer>().material; 
+        //현재 객체의 Component들을 참조해 Renderer라는 컴포넌트의 Material정보를 받아옵니다.
+    }
+
+    void Update () {
+        Vector2 newOffset = thisMaterial.mainTextureOffset;
+        // 새롭게 지정해줄 OffSet 객체를 선언합니다.
+        newOffset.Set(0, newOffset.y + (scrollSpeed * Time.deltaTime));
+        // Y부분에 현재 y값에 속도에 프레임 보정을 해서 더해줍니다.
+        thisMaterial.mainTextureOffset = newOffset;
+        //그리고 최종적으로 Offset값을 지정해줍니다.
+    }
+}
