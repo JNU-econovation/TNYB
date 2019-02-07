184787ea7b56a11451be3e34bce9f334f8bb3fa5
jbj616 <jbj616@gmail.com>
2019-02-08 01:17:14 +0900

    feat: 가구 추가및 함수

--- New Unity Project (7)/Assets/03_Scripts/MyRoom/RoomManager.cs
+++ New Unity Project (7)/Assets/03_Scripts/MyRoom/RoomManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class RoomManager : MonoBehaviour
 {
     public GameObject tile_yellow, tile_green, tile_blue, tile_gray;
-    public GameObject bed_blue, bed_red;
-    public GameObject table_1, table_2, tv_table, table_blue;
-    public GameObject sofa_1, sofa_2, sofa_blue, sofa2_blue;
+    public GameObject bed_blue, bed_red, bed_purple;
+    public GameObject pctable, table_2, tv_table, pctable_blue, pctable_red;
+    public GameObject sofa_red, sofa2_red, sofa_blue, sofa2_blue, sofa_green, sofa2_green;
     public GameObject computer, TV;
     public GameObject rug, trashcan, lamp, closet, library;
-    public GameObject picture, picture_red;
-    public GameObject chair_gray, chair_red;
+    public GameObject picture_yellow, picture_red, picture_blue;
+    public GameObject chair_gray, chair_red, chair_blue;
     public GameObject door;
+
+   
     public GameObject ItemInventory;
     public GameObject ButtonPanel;
     public GameObject SelectGamePanel;
@@ -70,4 +72,54 @@ public class RoomManager : MonoBehaviour
     {
         PurchasePanel.SetActive(false);
     }
+    //타일 설정 함수
+    public void SetTile_Yellow()
+    {
+        tile_yellow.SetActive(true);
+        tile_blue.SetActive(false);
+        tile_gray.SetActive(false);
+        tile_green.SetActive(false);
+    }
+    public void SetTile_Gray()
+    {
+        tile_yellow.SetActive(false);
+        tile_blue.SetActive(false);
+        tile_gray.SetActive(true);
+        tile_green.SetActive(false);
+    }
+    public void SetTile_Green()
+    {
+        tile_yellow.SetActive(false);
+        tile_blue.SetActive(false);
+        tile_gray.SetActive(false);
+        tile_green.SetActive(true);
+    }
+    public void SetTile_Blue()
+    {
+        tile_yellow.SetActive(false);
+        tile_blue.SetActive(true);
+        tile_gray.SetActive(false);
+        tile_green.SetActive(false);
+    }
+
+    //침대 설정 함수
+    public void SetBed_Blue()
+    {
+        bed_red.SetActive(false);
+        bed_blue.SetActive(true);
+        bed_purple.SetActive(false);
+    }
+    public void SetBed_Red()
+    {
+        bed_red.SetActive(true);
+        bed_blue.SetActive(false);
+        bed_purple.SetActive(false);
+    }
+    public void SetBed_PurPle()
+    {
+        bed_red.SetActive(false);
+        bed_blue.SetActive(false);
+        bed_purple.SetActive(true);
+    }
+
 }
