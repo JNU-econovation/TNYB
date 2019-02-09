44b5da684f9bd4fde9d9e43102d6e9868f8540d3
Cory Kim <corykim0829@gmail.com>
2019-02-10 03:05:51 +0900

    ui: time bar 10s left color

--- New Unity Project (7)/Assets/03_Scripts/04_Cashier/Timer.cs
+++ New Unity Project (7)/Assets/03_Scripts/04_Cashier/Timer.cs
@@ -10,6 +10,7 @@ public class Timer : MonoBehaviour {
 	private float timeLeft;
     public GameObject finishPanel;
 	private bool isResultShowed;
+	public string timeBarColor_10 = "FF3A06";
 
 	// Use this for initialization
 	void Start () {
@@ -29,7 +30,11 @@ public class Timer : MonoBehaviour {
 				isResultShowed = true;
 				StartCoroutine(GameManager.Instance.IeResultScoreEffect());
 			}
-			
+		}
+
+		if (timeLeft < 10)
+		{
+			timeBar.color = GetColorFromString(timeBarColor_10);
 		}
 		
         if (timeLeft > 0)
@@ -41,5 +46,24 @@ public class Timer : MonoBehaviour {
 		{
 			Time.timeScale = 0;
 		}
+	}
+
+	private int HexToDec(string hex)
+	{
+		int dec = System.Convert.ToInt32(hex, 16);
+		return dec;
+	}
+
+	private float HexToFloatNormalized(string hex)
+	{
+		return HexToDec(hex) / 255f;
+	}
+
+	private Color GetColorFromString(string HexString)
+	{
+		float red = HexToFloatNormalized(HexString.Substring(0, 2));
+		float green = HexToFloatNormalized(HexString.Substring(2, 2));
+		float blue = HexToFloatNormalized(HexString.Substring(4, 2));
+		return new Color(red, green, blue);
 	}
 }
