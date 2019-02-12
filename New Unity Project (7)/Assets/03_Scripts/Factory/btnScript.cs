4cc8e0a408610255d4e2dfd6ad8940b2db418829
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-02-13 04:54:58 +0900

    fix : sound2

--- New Unity Project (7)/Assets/03_Scripts/Factory/btnScript.cs
+++ New Unity Project (7)/Assets/03_Scripts/Factory/btnScript.cs
@@ -1,4 +1,3 @@
-
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +8,10 @@ public class btnScript : MonoBehaviour
 
     public Sprite btnSprite1;
     public Sprite btnSprite2;
-    public Button MusicBtn;
     // Start is called before the first frame update
     void Start()
     {
-        BtnClickEvent_Music();
-       // ClickEvent();
+        ClickEvent();
     }
 
     // Update is called once per frame
@@ -23,14 +20,11 @@ public class btnScript : MonoBehaviour
         
     }
 
-    public void BtnClickEvent_Music()
+    public void ClickEvent()
     {
-        if (FactoryManager.Instance.getMusic_bool())
-        {
-            MusicBtn.GetComponent<Image>().sprite = btnSprite1;
-        }
+        if (factorySfxManager.Instance.getisSfxMute())
+            GetComponent<Image>().sprite = btnSprite1;
         else
-
             GetComponent<Image>().sprite = btnSprite2;
     }
     public void BtnClickEvent_Music()
@@ -52,7 +46,6 @@ public class btnScript : MonoBehaviour
         {
             GetComponent<Image>().sprite = btnSprite2;
         }
-
     }
     public void BtnClickEvent_Music_Mart()
     {
