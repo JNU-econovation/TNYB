0763c4710a7704c619add87bdea5e0c298ff040e
Cory Kim <corykim0829@gmail.com>
2019-02-13 10:31:05 +0900

    feat: main transition

---
+++ New Unity Project (7)/Assets/03_Scripts/Main/main_TransitionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_TransitionManager : MonoBehaviour
+{
+    public GameObject transitionPanel;
+    public Animator transitionAnim;
+    public string sceneName;
+
+    private void Awake()
+    {
+        transitionPanel.SetActive(false);
+    }
+
+    public void toMain()
+    {
+        transitionPanel.SetActive(true);
+        SfxManager.Instance.playClick();
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
